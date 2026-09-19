a1e244814519784bcc93fbb05586f85f4b924a96
rupesh1820 <your-email@example.com>
2026-09-19 14:58:32 +0530

    ho ja bhai

--- Backend/Config/mailer.js
+++ Backend/Config/mailer.js
@@ -41,12 +41,11 @@ if (!process.env.EMAIL_USER || !process.env.EMAIL_PASS) {
 // ========================================
 // Gmail Transporter
 // ========================================
-
 const transporter = nodemailer.createTransport({
   host: "smtp.gmail.com",
   port: 587,
   secure: false,
-
+  family: 4,
   auth: {
     user: process.env.EMAIL_USER,
     pass: process.env.EMAIL_PASS,
